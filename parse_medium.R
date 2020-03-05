8c56944982aa1818f1b56b9a4a13c7be2fab9ac9
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-03-05 16:40:01 -0500

    build out functions to read downloaded html

--- parse_medium.R
+++ parse_medium.R
@@ -1,27 +1,80 @@
 library(rvest)
 library(jsonlite)
+library(lubridate)
 
+get_body_text <- function(article_html, collapse = "\n") {
+  article_html %>% 
+    xml_nodes(xpath = "//article//p|//article//h1|//article//h2|//article//h3|//article//figcaption") %>% 
+    xml_text() %>% 
+    str_c(collapse = collapse)
+}
 
+get_body_links <- function(article_html) {
+  article_html %>% 
+    xml_nodes(css = "p > a") %>% 
+    xml_attr("href")
+}
 
-article_html <- read_html(collected_articles[1, "html"])
-article_html <- read_html(collected_articles[2, "html"])
+get_article_metadata <- function(article_html) {
+  article_html %>% 
+    xml_node(xpath = "//script[@type='application/ld+json']") %>% 
+    xml_text() %>% 
+    fromJSON(flatten = FALSE)
+}
 
-body_text <- article_html %>% 
-  xml_nodes("p") %>% 
-  xml_text() %>% 
-  str_c(collapse = " ")
+get_article_date <- function(article_metadata) {
+  ymd_hms(article_metadata[["datePublished"]])
+}
 
-body_links <- article_html %>% 
-  xml_nodes("p > a") %>% 
-  xml_attr("href")
+get_article_author <- function(article_metadata) {
+  tibble(
+    author_type = article_metadata[["author"]][["@type"]],
+    author_name = article_metadata[["author"]][["name"]],
+    author_url = article_metadata[["author"]][["url"]]
+  )
+}
 
-article_data <- article_html %>% 
-  xml_nodes("script") %>% 
-  .[10] %>% 
-  xml_text() %>% 
-  str_sub(26) %>% 
-  fromJSON
+get_article_publisher <- function(article_metadata) {
+  tibble(
+    publisher_type = article_metadata[["publisher"]][["@type"]],
+    publisher_name = article_metadata[["publisher"]][["name"]],
+    publisher_url = article_metadata[["publisher"]][["url"]]
+  )
+}
 
-article_author <- article_data
+get_article_tags <- function(article_metadata) {
+  tibble(keywords = article_metadata[["keywords"]]) %>% 
+    separate(col = keywords, into = c("type", "keyword"), sep = ":")
+}
 
-jsonedit(article_data)
+get_article_title <- function(article_metadata) {
+  article_metadata[["name"]]
+}
+
+article_core_table <- function(url, html) {
+  article_html <- read_html(html)
+  md <- get_article_metadata(article_html)
+  authorship <- get_article_author(md)
+  publisher <- get_article_publisher(md)
+  one_row <- tibble(
+    url = url,
+    text = get_body_text(article_html),
+    date_published = get_article_date(md),
+    title = get_article_title(md),
+  ) %>% 
+    bind_cols(authorship, publisher)
+}
+
+article_tag_table <- function(url, html) {
+  article_html <- read_html(html)
+  md <- get_article_metadata(article_html)
+  tags <- get_article_tags(md)
+  tags %>% add_column(url = url, .before = TRUE)
+}
+
+article_links_table <- function(url, html) {
+  article_html <- read_html(html)
+  links <- get_body_links(article_html)
+  tibble(url = url, link = links) %>% 
+    mutate(external = str_detect(link, "^http"))
+}
--- scrape_medium.R
+++ scrape_medium.R
@@ -1,26 +1,32 @@
 library(tidyverse)
 library(DBI)
+library(rvest)
 library(RSQLite)
 library(progress)
-library(glue)
+
+source("parse_medium.R")
 
 url_list <- read_lines("links.txt")
 
 db <- dbConnect(SQLite(), "medium.sqlite3")
 dbExecute(db, "CREATE TABLE IF NOT EXISTS article(url TEXT UNIQUE NOT NULL, html TEXT NOT NULL)")
-
-# load links first time
 urls_to_fetch <- setdiff(url_list, dbGetQuery(db, "SELECT url FROM article")$url)
 
 upb <- progress_bar$new(format = "  downloading [:bar] :percent eta: :eta",
-                        total = length(urls_to_fetch), clear = FALSE, width= 60)
+                        total = length(urls_to_fetch), width = 60)
 walk(urls_to_fetch, function(url) {
-  upb$tick()
   htmlstring <- str_c(as.character(read_html(url)), collapse = "")
   dbAppendTable(db, "article", tibble(url = url, html = htmlstring))
-  })
+  upb$tick()
+})
 
 collected_articles <- dbReadTable(db, "article")
 
 dbDisconnect(db)
+
+core_table <- pmap_dfr(collected_articles, function(url, html) article_core_table(url, html))
+
+article_tags <- pmap_dfr(collected_articles, function(url, html) article_tag_table(url, html))
+
+article_links <- pmap_dfr(collected_articles, function(url, html) article_links_table(url, html))
 
